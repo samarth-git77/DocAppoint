eef8f22992c47434d6e59ac764987dfeb5163752
samarth-git77 <123804056+samarth-git77@users.noreply.github.com>
2024-09-09 14:19:43 +0530

    Update app.js
    cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL,"https://doc-appoint-alpha.vercel.app/"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
