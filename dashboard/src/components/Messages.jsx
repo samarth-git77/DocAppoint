1afe1e3de4a371d9294496000794cb9321e1c745
samarth-git77 <123804056+samarth-git77@users.noreply.github.com>
2024-09-10 21:37:05 +0530

    Update Messages.jsx vercel api added

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "https://docappoint-api.onrender.com/api/v1/message/getall",
+          "https://doc-appoint-api.vercel.app/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
